9e95b4eb223f7eb0774098431473299bd8b9d72f
markusjonek <markus.jonek@gmail.com>
2022-04-15 11:58:00 +0200

    Update subscriber.py

--- exc1/receiver/src/subscriber.py
+++ exc1/receiver/src/subscriber.py
@@ -7,7 +7,7 @@ def callback(data):
     q = 0.15
     heard = data.data
     new_number  = heard / q
-    second_pub = rospy.Publisher("/kthfs/result", Float32, queue_size=1)
+    second_pub = rospy.Publisher("kthfs/result", Float32, queue_size=1)
     second_pub.publish(new_number)
     #rospy.loginfo(new_number)
 
