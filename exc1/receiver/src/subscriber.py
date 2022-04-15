776761fe6eb373f5f899a4f350932b4737a4548f
markusjonek <markus.jonek@gmail.com>
2022-04-15 11:01:02 +0200

    second publish float instead of int

--- exc1/receiver/src/subscriber.py
+++ exc1/receiver/src/subscriber.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Int16
+from std_msgs.msg import Int16, Float32
 
 def callback(data):
     q = 0.15
     heard = data.data
     new_number  = heard / q
-    second_pub = rospy.Publisher("/kthfs/result", Int16, queue_size=1)
+    second_pub = rospy.Publisher("/kthfs/result", Float32, queue_size=1)
     second_pub.publish(new_number)
     #rospy.loginfo(new_number)
 
