f8a2fe16beadd652c8b216136f4710dbaf7d26df
markusjonek <markus.jonek@gmail.com>
2022-04-16 12:24:21 +0200

    class structure

--- exc2/plot.py
+++ exc2/plot.py
@@ -1,28 +1,36 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from itertools import count
 from matplotlib.animation import FuncAnimation
-import time
 from math import pi, e, sin
-plt.style.use('fivethirtyeight')
 
-x = []
-y = []
-index = count()
 
-def f(t):
-    return 3*pi*e**(-1*(5*sin(2*pi*t)))
+class Plot:
+    def __init__(self, x_min, x_max, y_min, y_max):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
+        self.x_values = []
+        self.y_values = []
+        self.index = count()
 
-def animate():
-    x1 = next(index)
-    x.append(x1)
-    y.append(f(x1*pi/180))
-    plt.cla()
-    plt.plot(x, y)
-    plt.xlim([0, 1000])
-    plt.ylim([-2, 1400])
+    def h(self, t):
+        return 3 * pi * e ** (-1 * (5 * sin(2 * pi * t)))
 
-ani = FuncAnimation(plt.gcf(), animate, interval=100)
+    def animate_helper(self, i):
+        t = next(self.index)
+        self.x_values.append(t)
+        self.y_values.append(self.h(t*pi/180))
+        plt.cla()
+        plt.plot(self.x_values, self.y_values)
+        plt.xlim([self.x_min, self.x_max])
+        plt.ylim([self.y_min, self.y_max])
 
-plt.tight_layout()
-plt.show()
+    def live_animation(self):
+        ani = FuncAnimation(plt.gcf(), self.animate_helper, interval=20)
+        plt.tight_layout()
+        plt.show()
+
+
+hej = Plot(0, 1000, 0, 1600)
+hej.live_animation()
